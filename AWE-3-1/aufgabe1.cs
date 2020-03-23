264a079c48edd6e9f41699b70b13c688f4e7b5ea
Jonathan Zimmerbeutel <J.Zimmerbeutel@lleyendecker.de>
2020-03-23 15:29:02 +0100

    Index Ausgabe Fenster angepasst

--- AWE-3-1/aufgabe1.cs
+++ AWE-3-1/aufgabe1.cs
@@ -20,19 +20,23 @@ namespace AWE_3_1
         private void btnA1aStart_Click(object sender, EventArgs e)
         {
             lstShow.Items.Clear();
+            lstA1Index.Items.Clear();
             for (int i = 10; i >= 0; i--)
             {
                 lstShow.Items.Add(Convert.ToString(i));
+                lstA1Index.Items.Add(Convert.ToString(i));
             }
         }
 
         private void btnA1bStart_Click(object sender, EventArgs e)
         {
             lstShow.Items.Clear();
+            lstA1Index.Items.Clear();
             for (int i = 5; i < 15; i++)
             {
                 double y = Math.Sqrt(i);
                 lstShow.Items.Add(Convert.ToString(y));
+                lstA1Index.Items.Add(Convert.ToString(i));
             }
         }
     }
