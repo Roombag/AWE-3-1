00271e064b399b6f8bcee242cbc62774b9528fe6
Jonathan Zimmerbeutel <J.Zimmerbeutel@lleyendecker.de>
2020-03-23 16:08:43 +0100

    GUI von Aufgabe 2 kopiert

--- AWE-3-1/Aufgabe3.Designer.cs
+++ AWE-3-1/Aufgabe3.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnA3Start = new System.Windows.Forms.Button();
+            this.txtA3Endwert = new System.Windows.Forms.TextBox();
+            this.lblA3Endwert = new System.Windows.Forms.Label();
+            this.lblA3Startwert = new System.Windows.Forms.Label();
+            this.txtA3Startwert = new System.Windows.Forms.TextBox();
+            this.lbxA3Ausgabe = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnA3Start
+            // 
+            this.btnA3Start.Location = new System.Drawing.Point(13, 13);
+            this.btnA3Start.Name = "btnA3Start";
+            this.btnA3Start.Size = new System.Drawing.Size(75, 23);
+            this.btnA3Start.TabIndex = 0;
+            this.btnA3Start.Text = "Start";
+            this.btnA3Start.UseVisualStyleBackColor = true;
+            // 
+            // txtA3Endwert
+            // 
+            this.txtA3Endwert.Location = new System.Drawing.Point(65, 71);
+            this.txtA3Endwert.Name = "txtA3Endwert";
+            this.txtA3Endwert.Size = new System.Drawing.Size(76, 20);
+            this.txtA3Endwert.TabIndex = 10;
+            // 
+            // lblA3Endwert
+            // 
+            this.lblA3Endwert.AutoSize = true;
+            this.lblA3Endwert.Location = new System.Drawing.Point(10, 74);
+            this.lblA3Endwert.Name = "lblA3Endwert";
+            this.lblA3Endwert.Size = new System.Drawing.Size(49, 13);
+            this.lblA3Endwert.TabIndex = 9;
+            this.lblA3Endwert.Text = "Endwert:";
+            // 
+            // lblA3Startwert
+            // 
+            this.lblA3Startwert.AutoSize = true;
+            this.lblA3Startwert.Location = new System.Drawing.Point(10, 49);
+            this.lblA3Startwert.Name = "lblA3Startwert";
+            this.lblA3Startwert.Size = new System.Drawing.Size(52, 13);
+            this.lblA3Startwert.TabIndex = 8;
+            this.lblA3Startwert.Text = "Startwert:";
+            // 
+            // txtA3Startwert
+            // 
+            this.txtA3Startwert.Location = new System.Drawing.Point(65, 46);
+            this.txtA3Startwert.Name = "txtA3Startwert";
+            this.txtA3Startwert.Size = new System.Drawing.Size(76, 20);
+            this.txtA3Startwert.TabIndex = 7;
+            // 
+            // lbxA3Ausgabe
+            // 
+            this.lbxA3Ausgabe.FormattingEnabled = true;
+            this.lbxA3Ausgabe.Location = new System.Drawing.Point(13, 97);
+            this.lbxA3Ausgabe.Name = "lbxA3Ausgabe";
+            this.lbxA3Ausgabe.Size = new System.Drawing.Size(128, 186);
+            this.lbxA3Ausgabe.TabIndex = 6;
+            // 
+            // Aufgabe3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtA3Endwert);
+            this.Controls.Add(this.lblA3Endwert);
+            this.Controls.Add(this.lblA3Startwert);
+            this.Controls.Add(this.txtA3Startwert);
+            this.Controls.Add(this.lbxA3Ausgabe);
+            this.Controls.Add(this.btnA3Start);
+            this.Name = "Aufgabe3";
             this.Text = "Aufgabe3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnA3Start;
+        private System.Windows.Forms.TextBox txtA3Endwert;
+        private System.Windows.Forms.Label lblA3Endwert;
+        private System.Windows.Forms.Label lblA3Startwert;
+        private System.Windows.Forms.TextBox txtA3Startwert;
+        private System.Windows.Forms.ListBox lbxA3Ausgabe;
     }
 }
