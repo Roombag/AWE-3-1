0c769385c44aef5d6224ba65d1b13dd22d22a999
Jonathan Zimmerbeutel <J.Zimmerbeutel@lleyendecker.de>
2020-03-24 14:35:31 +0100

    Kommentare eingefügt

--- AWE-3-1/Aufgabe5.cs
+++ AWE-3-1/Aufgabe5.cs
@@ -26,9 +26,11 @@ namespace AWE_3_1
             // Intervallgrenzen Oben und Unten
             double eingabe = Convert.ToDouble(txtA5Eingabe.Text);
             double q1 = 0, q2 = 0;
+            // aktuelle quadrierte Zahl und zuletzt quadrierte Zahl
             double zwErgebnis = 0;
             // Rechenvariable
             double intSchritt = 1;
+            // schrittgröße des aktuellen Intervalls
             do
             {
                 do
@@ -36,9 +38,12 @@ namespace AWE_3_1
                     q2 = q1;
                     q1 = Math.Round(intSchritt + q1, 15);
                     zwErgebnis = Math.Round(q1 * q1, 15);
+                    // schleift durch alle zahlen im intervall mit dem aktuellen intervallschritt
                 } while (zwErgebnis < eingabe);
+                // solange bis eine größere Zahl als die eingegebene gefunden wird
                 intUnten = q2;
                 intOben = q1;
+                // interval neu setzen (haupsächlich zur Übersicht und zum Debuggen)
                 q1 = intUnten;
                 q2 = intOben;
                 lbxA5Ausgabe.Items.Add(
@@ -50,14 +55,16 @@ namespace AWE_3_1
                     "Intevall: " +
                     Convert.ToString(intSchritt)
                     );
+                // Ausgabe in ListBox
                 intSchritt = intSchritt / 10;
+                // Schrittgröße für nächsten Schleifendurchlauf ändern
             } while (zwErgebnis != eingabe);
-
+            // solange bis der richtige Wert gefunden wurde
             lbxA5Ausgabe.Items.Add(
                 "Ergebnis: " +
                 Convert.ToString(q2)
                 );
-
+            // Ausgabe in ListBox
         }
     }
 }
